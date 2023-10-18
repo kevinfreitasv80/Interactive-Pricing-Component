238d8279b75df8dc570786431bb96005a10964f1
kevinfreitasv80 <kevinfreitasv80@gmail.com>
2023-10-18 13:19:06 -0300

    Interactive pricing component

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 const spanPageviews = document.getElementById("spanPageviews");
+const discountSpan = document.querySelector(".discount");
 const strong = document.getElementById("strong");
 const range = document.querySelector(".range");
 const check = document.getElementById("check");
@@ -21,6 +22,13 @@ const changePrice = () => {
             break;
     }
 }
+const updateSliderColor = () => {
+    const percentage = (range.value - range.min) / (range.max - range.min) * 100;
+    range.style.background = `linear-gradient(to right, #A6F1E9 ${percentage}%, #EDF1FB ${percentage}%)`;
+}
 
+updateSliderColor();
 range.addEventListener("change", () => changePrice());
 check.addEventListener("click", () => changePrice());
+range.addEventListener("input", () => updateSliderColor());
+window.addEventListener("resize", () => window.innerWidth < 505 ? discountSpan.textContent = "-25%" : discountSpan.textContent = "25% discount");
